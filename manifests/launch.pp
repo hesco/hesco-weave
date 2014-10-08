a235d479fe587f61317c55e37e0512db5c20856b
Hugh Esco <hesco@yourmessagedelivered.com>
2014-10-08 07:26:20 -0400

    validate $docker_host_weave_ip, pull tokens from weave::params

--- manifests/launch.pp
+++ manifests/launch.pp
@@ -1,13 +1,18 @@
 
-define weave::launch ( $ip, $peers ){
+define weave::launch ( 
+  $docker_host_weave_ip,
+  $docker_cluster_peers,
+){
 
-  $weave = hiera('ymd_docker::network::weave::script', '/usr/local/bin/weave')
-  $weave_image = hiera('ymd_docker::network::weave::image', 'zettio/weave')
-  $weave_container = hiera('ymd_docker::network::weave::container', 'weave')
+  validate_bool( is_ip_address( $docker_host_weave_ip ) )
 
-  exec { "weave_launch_$ip":
-    command => "$weave launch $ip $peers ",
-     unless => "/usr/bin/docker inspect -f '{{ .Image }}' $weave_container 2>&1 | /bin/grep -v '^Error\|<no value>' -q",
+  $docker = getvar('weave::docker')
+  $weave = getvar('weave::weave')
+  $weave_container = getvar('weave::weave_container')
+
+  exec { "weave_launch_$docker_host_weave_ip":
+    command => "$weave launch $docker_host_weave_ip $docker_cluster_peers ",
+     unless => "$docker inspect -f '{{ .Image }}' $weave_container 2>&1 | /bin/grep -v '^Error\|<no value>' -q",
     timeout => 600,
   }
 
