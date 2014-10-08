b337aa38bb04955ab20f5c04c2d7463c56d609fa
Hugh Esco <hesco@yourmessagedelivered.com>
2014-10-08 03:18:42 -0400

    Fix unless attribute to Exec["weave_launch_$ip"]; pull hiera data

--- manifests/launch.pp
+++ manifests/launch.pp
@@ -1,12 +1,13 @@
 
-define weave::launch ( $ip, $peers ){ "":
+define weave::launch ( $ip, $peers ){
 
-  $weave = '/usr/local/bin/weave'
-  $weave_image = 'zettio/weave'
+  $weave = hiera('ymd_docker::network::weave::script', '/usr/local/bin/weave')
+  $weave_image = hiera('ymd_docker::network::weave::image', 'zettio/weave')
+  $weave_container = hiera('ymd_docker::network::weave::container', 'weave')
 
-  exec { "weave launch $ip $peers":
+  exec { "weave_launch_$ip":
     command => "$weave launch $ip $peers ",
-     unless => "/usr/bin/docker inspect -f "{{ .Image }}" $weave_image 2>&1 | /bin/grep -v ^Error -q",
+     unless => "/usr/bin/docker inspect -f '{{ .Image }}' $weave_container 2>&1 | /bin/grep -v '^Error\|<no value>' -q",
     timeout => 600,
   }
 
