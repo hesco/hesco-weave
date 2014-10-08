49d98f39a8afc80215240293990fad385f64606e
Hugh Esco <hesco@yourmessagedelivered.com>
2014-10-08 04:52:53 -0400

    Adds data validation to weave::run defined type

--- manifests/run.pp
+++ manifests/run.pp
@@ -4,6 +4,12 @@ define weave::run ( $ip, $image, $options ){
   $docker = hiera('ymd_docker::network::weave::docker', '/usr/bin/docker')
   $weave = hiera('ymd_docker::network::weave::script', '/usr/local/bin/weave')
 
+  validate_absolute_path( $weave )
+  validate_absolute_path( $docker )
+  validate_string( $image )
+  validate_string( $options )
+  validate_bool( is_ip_address( $ip ) )
+
   exec { "weave run $ip $image":
     command => "$weave run $ip $options $image",
      unless => "$docker inspect -f '{{ .Image }}' $image 2>&1 | /bin/grep -v '^Error\|<no value>' -q ",
