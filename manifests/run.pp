a57ea22868c3104f0e8841d9cef5a06daff24700
Hugh Esco <hesco@yourmessagedelivered.com>
2014-10-10 02:04:58 -0400

    weave::run's Exec["weave run $ip $image"] requires running weave router

--- manifests/run.pp
+++ manifests/run.pp
@@ -3,6 +3,7 @@ define weave::run ( $ip, $image, $options ){
 
   $docker = hiera('weave::docker', '/usr/bin/docker')
   $weave = hiera('weave::script', '/usr/local/bin/weave')
+  $docker_host_weave_ip = hiera('weave::docker_host_weave_ip', undef)
 
   validate_absolute_path( $weave )
   validate_absolute_path( $docker )
@@ -14,6 +15,8 @@ define weave::run ( $ip, $image, $options ){
     command => "$weave run $ip $options $image",
      unless => "$docker inspect -f '{{ .Image }}' $image 2>&1 | /bin/grep -v '^Error\|<no value>' -q ",
     timeout => 600,
+     require => [ Exec["weave_launch_$docker_host_weave_ip"],
+                  Exec["restart_weave_for_$docker_host_weave_ip"] ],
   }
 
 }
