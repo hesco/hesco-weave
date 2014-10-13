c805ba5e86e2318e49e6d9f4f5169a29e24dff71
Hugh Esco <hesco@campaignfoundations.com>
2014-10-13 17:31:39 -0400

    Resolve Bug #7, weave::run ( $host, $ip, $image, $options ), INTERFACE CHANGE
    This adds a new $host parameter to the interface of this defined type.
    Necessary to fix bug in unless => attribute

--- manifests/run.pp
+++ manifests/run.pp
@@ -1,5 +1,5 @@
 
-define weave::run ( $ip, $image, $options ){
+define weave::run ( $host, $ip, $image, $options ){
 
   $docker = hiera('weave::docker', '/usr/bin/docker')
   $weave = hiera('weave::script', '/usr/local/bin/weave')
@@ -13,7 +13,7 @@ define weave::run ( $ip, $image, $options ){
 
   exec { "weave run $ip $image":
     command => "$weave run $ip $options $image",
-     unless => "$docker inspect -f '{{ .Image }}' $image 2>&1 | /bin/grep -v '^Error\|<no value>' -q ",
+     unless => "$docker inspect -f '{{ .Image }}' $host 2>&1 | /bin/grep -v '^Error\|<no value>' -q ",
     timeout => 600,
      require => [ Exec["weave_launch_$docker_host_weave_ip"],
                   Exec["restart_weave_for_$docker_host_weave_ip"] ],
