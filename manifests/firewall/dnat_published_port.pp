b9959ab5bdc3a511df35fac121f46df20e8b7b4b
Hugh Esco <hesco@campaignfoundations.com>
2014-11-11 21:36:45 +0000

    account for ports published to specific public ip

--- manifests/firewall/dnat_published_port.pp
+++ manifests/firewall/dnat_published_port.pp
@@ -2,7 +2,10 @@
 # -A FORWARD -d 172.17.0.7/32 ! -i docker0 -o docker0 -p tcp -m tcp --dport 5432 -j ACCEPT
 # -A DOCKER ! -i docker0 -p tcp -m tcp --dport 5432 -j DNAT --to-destination 172.17.0.7:5432
 
-define weave::firewall::dnat_published_port ( $container_ip, $published_port, $protocol ) {
+# -A FORWARD -d 172.17.0.2/32 ! -i docker0 -o docker0 -p tcp -m tcp --dport 25 -j ACCEPT
+# -A DOCKER -d 68.168.146.146/32 ! -i docker0 -p tcp -m tcp --dport 25 -j DNAT --to-destination 172.17.0.2:25
+
+define weave::firewall::dnat_published_port ( $container_ip, $published_port, $protocol, $public_ip ) {
 
   firewall { "$rule_id FORWARD $published_port for $container_ip":
           chain => 'FORWARD',
@@ -14,14 +17,27 @@ define weave::firewall::dnat_published_port ( $container_ip, $published_port, $p
          action => accept,
   }
 
-  firewall { "$rule_id dnat $published_port for $container_ip":
-      table => 'nat',
-      chain => 'DOCKER',
-    iniface => '! docker0',
-      proto => [ $protocol ],
-      dport => [ $published_port ],
-     todest => "$container_ip:$published_port",
-       jump => 'DNAT',
+  if $public_ip {
+    firewall { "$rule_id dnat $published_port for $container_ip":
+          table => 'nat',
+          chain => 'DOCKER',
+    destination => $public_ip,
+        iniface => '! docker0',
+          proto => [ $protocol ],
+          dport => [ $published_port ],
+         todest => "$container_ip:$published_port",
+           jump => 'DNAT',
+    }
+  else {
+    firewall { "$rule_id dnat $published_port for $container_ip":
+        table => 'nat',
+        chain => 'DOCKER',
+      iniface => '! docker0',
+        proto => [ $protocol ],
+        dport => [ $published_port ],
+       todest => "$container_ip:$published_port",
+         jump => 'DNAT',
+    }
   }
 
 }
